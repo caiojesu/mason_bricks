832783d193aebb3ca76a446cd80b33111c49f0e0
Caio Jesus <caioj3505@gmail.com>
2023-03-16 11:33:23 -0300

    feat(domain_driven_feature): add build_runner code generation process in post gen hook

--- domain_driven_feature/hooks/post_gen.dart
+++ domain_driven_feature/hooks/post_gen.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add post-generation logic.
+void run(HookContext context) async {
+  final codeGen = context.logger.progress('Build runner gen in progress');
+  await Process.run(
+    'flutter',
+    ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'],
+    runInShell: true,
+  );
+  codeGen.complete();
 }
