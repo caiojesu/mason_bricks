db6343c3e5c0a0f6807380a01ca9848d60ebae92
Caio Jesus <caioj3505@gmail.com>
2023-03-16 11:59:18 -0300

    feat(domain_driven_feature): makes the use of json_serializable conditional

--- domain_driven_feature/__brick__/lib/features/{{feature_name.snakeCase()}}/data/models/{{feature_name.snakeCase()}}_model.dart
+++ domain_driven_feature/__brick__/lib/features/{{feature_name.snakeCase()}}/data/models/{{feature_name.snakeCase()}}_model.dart
@@ -1,19 +1,22 @@
-import 'package:json_annotation/json_annotation.dart';
-
 import '../../domain/entities/{{ feature_name.snakeCase() }}_entity.dart';
-
+{{#use_json_serializable}}
+import 'package:json_annotation/json_annotation.dart';
 part '{{ feature_name.snakeCase() }}_model.g.dart';
-
-@JsonSerializable()
+{{/use_json_serializable}}
+{{#use_json_serializable}}
+@JsonSerializable(){{/use_json_serializable}}
 class {{feature_name.pascalCase()}}Model extends {{feature_name.pascalCase()}}Entity{
   //TODO: Populate model
 
   {{feature_name.pascalCase()}}Model() {}
 
+{{#use_json_serializable}}
   factory {{feature_name.pascalCase()}}Model.fromJson(Map<String, dynamic> json) =>
     _${{feature_name.pascalCase()}}ModelFromJson(json);
 
   Map<String, dynamic> toJson() => _${{feature_name.pascalCase()}}ModelToJson(this);
+
+  {{/use_json_serializable}}
 
   {{feature_name.pascalCase()}}Entity toEntity() => {{feature_name.pascalCase()}}Entity();
 }
--- domain_driven_feature/hooks/post_gen.dart
+++ domain_driven_feature/hooks/post_gen.dart
@@ -2,11 +2,14 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  final codeGen = context.logger.progress('Build runner gen in progress');
-  await Process.run(
-    'flutter',
-    ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'],
-    runInShell: true,
-  );
-  codeGen.complete();
+  final useJsonSerializable = context.vars['use_json_serializable'];
+  if (useJsonSerializable) {
+    final codeGen = context.logger.progress('Build runner gen in progress');
+    await Process.run(
+      'flutter',
+      ['pub', 'run', 'build_runner', 'build', '--delete-conflicting-outputs'],
+      runInShell: true,
+    );
+    codeGen.complete();
+  }
 }
